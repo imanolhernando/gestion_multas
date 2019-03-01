c3e31b0f911ae1ff475ef9242e5a935a2ba65f1c
imanolhernando <imahernando@gmail.com>
2019-03-01 13:31:37 +0100

    correciones coche_get_matricula arreglado

--- modelo/sql/copiaDBdgt.sql
+++ modelo/sql/copiaDBdgt.sql
@@ -1,10 +1,10 @@
 CREATE DATABASE  IF NOT EXISTS `dgt` /*!40100 DEFAULT CHARACTER SET utf8 */;
 USE `dgt`;
--- MySQL dump 10.13  Distrib 8.0.13, for Win64 (x86_64)
+-- MySQL dump 10.13  Distrib 8.0.12, for Win64 (x86_64)
 --
 -- Host: localhost    Database: dgt
 -- ------------------------------------------------------
--- Server version	8.0.13
+-- Server version	8.0.12
 
 /*!40101 SET @OLD_CHARACTER_SET_CLIENT=@@CHARACTER_SET_CLIENT */;
 /*!40101 SET @OLD_CHARACTER_SET_RESULTS=@@CHARACTER_SET_RESULTS */;
@@ -156,6 +156,46 @@ SET character_set_client = @saved_cs_client;
 --
 -- Dumping routines for database 'dgt'
 --
+/*!50003 DROP FUNCTION IF EXISTS `nombre_mes` */;
+/*!50003 SET @saved_cs_client      = @@character_set_client */ ;
+/*!50003 SET @saved_cs_results     = @@character_set_results */ ;
+/*!50003 SET @saved_col_connection = @@collation_connection */ ;
+/*!50003 SET character_set_client  = utf8mb4 */ ;
+/*!50003 SET character_set_results = utf8mb4 */ ;
+/*!50003 SET collation_connection  = utf8mb4_0900_ai_ci */ ;
+/*!50003 SET @saved_sql_mode       = @@sql_mode */ ;
+/*!50003 SET sql_mode              = 'STRICT_TRANS_TABLES,NO_ENGINE_SUBSTITUTION' */ ;
+DELIMITER ;;
+CREATE DEFINER=`root`@`localhost` FUNCTION `nombre_mes`(numeroMes INTEGER) RETURNS varchar(50) CHARSET utf8
+    READS SQL DATA
+    DETERMINISTIC
+BEGIN
+
+DECLARE mes VARCHAR (20) DEFAULT 'NO EXISTE';
+
+CASE  numeroMes
+WHEN 1 then set MES = 'ENERO';
+WHEN 2 then set MES = 'FEBRERO';
+WHEN 3 then set MES = 'MARZO';
+WHEN 4 then set MES = 'ABRIL';
+WHEN 5 then set MES = 'MAYO';
+WHEN 6 then set MES = 'JUNIO';
+WHEN 7 then set MES = 'JULIO';
+WHEN 8 then set MES = 'AGOSTO';
+WHEN 9 then set MES = 'SEPTIEMBRE';
+WHEN 10 then set MES = 'OCTUBRE';
+WHEN 11 then set MES = 'NOVIEMBRE';
+WHEN 12 then set MES = 'DICIEMBRE';
+ELSE
+SET mes = concat(numeroMes, 'NO existe');
+end case;
+RETURN mes;
+END ;;
+DELIMITER ;
+/*!50003 SET sql_mode              = @saved_sql_mode */ ;
+/*!50003 SET character_set_client  = @saved_cs_client */ ;
+/*!50003 SET character_set_results = @saved_cs_results */ ;
+/*!50003 SET collation_connection  = @saved_col_connection */ ;
 /*!50003 DROP PROCEDURE IF EXISTS `agente_getById` */;
 /*!50003 SET @saved_cs_client      = @@character_set_client */ ;
 /*!50003 SET @saved_cs_results     = @@character_set_results */ ;
@@ -259,7 +299,7 @@ DELIMITER ;
 DELIMITER ;;
 CREATE DEFINER=`root`@`localhost` PROCEDURE `coche_get_matricula`(IN matricula VARCHAR(10))
 BEGIN
-SELECT id, matricula, modelo, km FROM coche WHERE matricula =matricula;
+SELECT id, matricula, modelo, km FROM coche WHERE coche.matricula = matricula;
 END ;;
 DELIMITER ;
 /*!50003 SET sql_mode              = @saved_sql_mode */ ;
@@ -316,6 +356,25 @@ INSERT INTO multa (importe, concepto, id_coche, id_agente) VALUES (p_importe, p_
 
 SET o_id = LAST_INSERT_ID();
 
+END ;;
+DELIMITER ;
+/*!50003 SET sql_mode              = @saved_sql_mode */ ;
+/*!50003 SET character_set_client  = @saved_cs_client */ ;
+/*!50003 SET character_set_results = @saved_cs_results */ ;
+/*!50003 SET collation_connection  = @saved_col_connection */ ;
+/*!50003 DROP PROCEDURE IF EXISTS `multa_objetivos` */;
+/*!50003 SET @saved_cs_client      = @@character_set_client */ ;
+/*!50003 SET @saved_cs_results     = @@character_set_results */ ;
+/*!50003 SET @saved_col_connection = @@collation_connection */ ;
+/*!50003 SET character_set_client  = utf8mb4 */ ;
+/*!50003 SET character_set_results = utf8mb4 */ ;
+/*!50003 SET collation_connection  = utf8mb4_0900_ai_ci */ ;
+/*!50003 SET @saved_sql_mode       = @@sql_mode */ ;
+/*!50003 SET sql_mode              = 'STRICT_TRANS_TABLES,NO_ENGINE_SUBSTITUTION' */ ;
+DELIMITER ;;
+CREATE DEFINER=`root`@`localhost` PROCEDURE `multa_objetivos`()
+BEGIN
+SELECT * FROM dgt.v_objetivos;
 END ;;
 DELIMITER ;
 /*!50003 SET sql_mode              = @saved_sql_mode */ ;
@@ -395,4 +454,4 @@ DELIMITER ;
 /*!40101 SET COLLATION_CONNECTION=@OLD_COLLATION_CONNECTION */;
 /*!40111 SET SQL_NOTES=@OLD_SQL_NOTES */;
 
--- Dump completed on 2019-02-28 20:09:46
+-- Dump completed on 2019-03-01 13:30:52
