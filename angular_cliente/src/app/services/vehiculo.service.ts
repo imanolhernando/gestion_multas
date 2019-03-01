7a69f39187e2065b6373a6ee5b302d265998d04e
imanolhernando <imahernando@gmail.com>
2019-03-01 12:50:27 +0100

    provider o service de vehiculo funcionando obtener vehiculo por matricula

---
+++ angular_cliente/src/app/services/vehiculo.service.ts
@@ -0,0 +1,25 @@
+import { Injectable } from '@angular/core';
+import { HttpClient } from '@angular/common/http';
+import { Observable } from 'rxjs';
+import { Agente } from '../model/agente';
+
+@Injectable({
+  providedIn: 'root'
+})
+export class VehiculoService {
+
+  private endpoint ='http://localhost:8080/wsrest/api/';
+
+  constructor(private httpClient: HttpClient) { 
+    console.trace('CocheService canActivate');
+  }
+
+  
+  getVehiculoByMatricula(matricula:string): Observable<any>{
+    let url = this.endpoint + `coche/${matricula}`;
+    console.trace(`CocheService getCocheByMatricula url: ${url}`);
+    return this.httpClient.get(url);
+  }
+
+
+}
