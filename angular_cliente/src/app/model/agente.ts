cf79d9390ca33d5e517d401ed65ed3d4b7285047
imanol <imahernando@gmail.com>
2019-03-04 08:25:17 +0100

    maquetacion y correciones funcionando login, listado multas, insertar multa

--- angular_cliente/src/app/model/agente.ts
+++ angular_cliente/src/app/model/agente.ts
@@ -10,39 +10,39 @@ export class Agente {
         this._id = id;
         this._apellido = apellido;
         this._placa = placa;
-        this._password = password;
+        this._password = "ABC";
         this._id = id;
         this._departamento = departamento;
     }
-        
+
     public get id(): number {
         return this._id;
     }
     public set id(value: number) {
         this._id = value;
     }
-    
+
     public get apellido(): string {
         return this._apellido;
     }
     public set apellido(value: string) {
         this._apellido = value;
     }
-    
+
     public get placa(): number {
         return this._placa;
     }
     public set placa(value: number) {
         this._placa = value;
     }
-    
+
     public get password(): string {
         return this._password;
     }
     public set password(value: string) {
         this._password = value;
     }
-    
+
     public get departamento(): string {
         return this._departamento;
     }
